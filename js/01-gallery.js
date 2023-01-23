b1666b68f0d2f27c4eeb90e49559453faa264757
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-01-23 16:31:27 +0200

    Adds galleries to task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function createImageCardsMarkup(images) {
+  return images.map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+  }).join('');
+};
+
+function onGalleryContainerClick(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  event.preventDefault();
+  const srcOriginal = event.target.dataset.source;
+  basicLightbox.create(`
+    <img src="${srcOriginal}" width="1280px">
+  `).show()
+
+  // const swatchEl = event.target;
+  // const parentImageCard = swatchEl.closest('gallery-item');
+  // parentImageCard
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function createImageCardsMarkup(images) {
+  return images.map(({ preview, original, description }) => {
+    return `
+ <li>
+ <a class="gallery__item" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+ </a>
+</li>
+`;
+  }).join('');
+};
+
+function onGalleryContainerClick(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+  return;
+  }
+  event.preventDefault();
+  const srcOriginal = event.target.dataset.source;
+  new SimpleLightbox('.gallery a', { /* options */ }); 
+  };
