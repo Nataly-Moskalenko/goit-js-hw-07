e0286799e7ca44b768183b057aa6d0248b440a5d
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-22 16:41:50 +0200

    Update 01-gallery.js
    Fixes closeGalleryOnEscape, removes eventListener after gallery closing

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,25 +3,24 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createImageCardsMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-galleryContainer.addEventListener('click', onGalleryContainerClick);
-
 function createImageCardsMarkup(images) {
-  return images.map(({ preview, original, description }) => {
-    return `
-<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
-`;
-  }).join('');
-};
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        `;
+    })
+    .join('');
+}
 
 function onGalleryContainerClick(event) {
   if (!event.target.classList.contains('gallery__image')) {
@@ -32,12 +31,19 @@ function onGalleryContainerClick(event) {
   const instance = basicLightbox.create(`
     <img src="${srcOriginal}" width="1280px" />
   `);
-  instance.show();
 
-  document.addEventListener("keydown", event => {
+  const closeGalleryOnEscape = (event) => {
     event.preventDefault();
-    if (event.code === 'Escape') {
+    if (event.code === 'Escape' && instance.visible()) {
       instance.close();
-    }    
-});
-};
+      document.removeEventListener('keydown', closeGalleryOnEscape);
+    }
+  };
+
+  instance.show(() => {
+    document.addEventListener('keydown', closeGalleryOnEscape);
+  });
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
