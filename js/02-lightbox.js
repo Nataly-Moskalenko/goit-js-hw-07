f31309bb6d1577e3ad45a8fb572a000bc58e0ac3
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-22 18:46:42 +0200

    Update 02-lightbox.js
    Fixes the starting of new SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,13 +8,13 @@ galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 function createImageCardsMarkup(images) {
   return images.map(({ preview, original, description }) => {
     return `
- <li>
- <a class="gallery__item" href="${original}">
-   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
- </a>
-</li>
-`;
+      <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+      </a>
+      </li>
+      `;
   }).join('');
 };
 
-const gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, });
+new SimpleLightbox('.gallery a', { captionDelay: 250, });
